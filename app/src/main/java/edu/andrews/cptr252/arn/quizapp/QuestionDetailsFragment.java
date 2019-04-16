28cf2e037a8636523b5514004fe113d9eaf4a15b
Daniel <danielarn1799@gmail.com>
2019-04-16 19:45:49 -0400

    Added references to Details Fragment edit text and buttons Added code to save information on changes

--- app/src/main/java/edu/andrews/cptr252/arn/quizapp/QuestionDetailsFragment.java
+++ app/src/main/java/edu/andrews/cptr252/arn/quizapp/QuestionDetailsFragment.java
@@ -5,9 +5,12 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 
@@ -21,13 +24,12 @@ public class QuestionDetailsFragment extends Fragment {
     /** Key used to pass the id of a question */
     public static final String EXTRA_QUESTION_ID = "edu.andrews.cptr252.arn.quizapp.question_id";
 
-    /** Tag for logging fragment messages */
-    public static final String TAG = "QuestionDetailsFragment";
-
     /** Reference to question field */
     private EditText mQuestionField;
     /** Reference to answer radio group */
     private RadioGroup mAnswerRadioGroup;
+    /** Reference to Done button */
+    private Button mDoneButton;
     /** Question that is being viewed/edited */
     private Question mQuestion;
 
@@ -93,7 +95,51 @@ public class QuestionDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_question_details, container, false);
+        View v = inflater.inflate(R.layout.fragment_question_details, container, false);
+
+        // get reference to EditText box for question
+        mQuestionField = v.findViewById(R.id.question_text);
+        mQuestionField.setText(mQuestion.getQuestion());
+        mQuestionField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // user typed text, update the question
+                mQuestion.setQuestion(s.toString());
+                updateQuestion();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // intentionally left blank
+            }
+        });
+
+        // get reference to RadioGroup box for question answer
+        mAnswerRadioGroup = v.findViewById(R.id.answer_radioGroup);
+        if (mQuestion.getQuestion() == null) {
+            mAnswerRadioGroup.check(-1);
+        } else {
+            mAnswerRadioGroup.check(mQuestion.getAnswer() ? R.id.true_radioButton : R.id.false_radioButton);
+        }
+        mAnswerRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // set the question's answer
+                if (checkedId == R.id.true_radioButton){
+                    mQuestion.setAnswer(true);
+                } else if (checkedId == R.id.false_radioButton){
+                    mQuestion.setAnswer(false);
+                }
+                updateQuestion();
+            }
+        });
+
+        return v;
     }
 
     /**
